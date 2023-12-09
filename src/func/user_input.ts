70df8ba217957100689f7700a8953b11e62657a7
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-09 12:23:28 +0900

    新関数追加

---
+++ src/func/user_input.ts
@@ -0,0 +1,18 @@
+
+/**
+ * ユーザーが入力した数値を指定した形式で整形する
+ * @param value ユーザーが入力した値
+ * @param min 最小値
+ * @param max 最大値
+ * @param integer 整数(規定値: true)
+ */
+export const input_format = (value: number, min: number, max: number, integer: boolean = true) => {
+	if (value < min) return min;
+	else if (value > max) return max;
+
+	if (integer) {
+		return Math.round(value);
+	} else {
+		return value;
+	}
+}
