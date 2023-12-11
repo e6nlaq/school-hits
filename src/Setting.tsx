d9a1787b51e6edacbbacad24b4fd43ccc5da130f
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-11 17:33:10 +0900

    🐛 有効期限設定、出席番号のバグ修正

--- src/Setting.tsx
+++ src/Setting.tsx
@@ -4,8 +4,10 @@ import { useCookies } from 'react-cookie';
 import React from 'react';
 
 import './css/App.css'
-import { init_cookie } from './func/cookie';
 import { input_format } from './func/user_input';
+import { cookie_date } from './variable/cookie';
+
+const cookie_list = ["dp_count", "user_number", "class_count"];
 
 // input_formatの簡略化
 const format = (value: React.ChangeEvent<HTMLInputElement>, integer: boolean = true): number => {
@@ -13,8 +15,7 @@ const format = (value: React.ChangeEvent<HTMLInputElement>, integer: boolean = t
 }
 
 const Setting = () => {
-	init_cookie();
-	const [cookie, setCookie, removeCookie] = useCookies(["dp_count", "user_number", "class_count"]);
+	const [cookie, setCookie, removeCookie] = useCookies(cookie_list);
 
 	return <>
 		<Helmet>
@@ -25,15 +26,15 @@ const Setting = () => {
 
 		<label htmlFor="dp_count">計算回数</label>
 		<input type="number" id="dp_count" value={cookie.dp_count} min={1} max={10} onChange={(event) => {
-			setCookie("dp_count", format(event));
+			setCookie("dp_count", format(event), { maxAge: cookie_date });
 		}} />
 
 		<br />
 
 		<label htmlFor="user_number">あなたの出席番号</label>
-		<input type="number" id="user_number" value={cookie.user_number} min={1} max={1000} onChange={
+		<input type="number" id="user_number" value={cookie.user_number} min={1} max={Number(cookie.class_count)} disabled={cookie.class_count === undefined} onChange={
 			(event) => {
-				setCookie("user_number", format(event));
+				setCookie("user_number", format(event), { maxAge: cookie_date });
 			}
 		} />
 
@@ -42,7 +43,8 @@ const Setting = () => {
 		<label htmlFor="class_count">クラスの人数</label>
 		<input type="number" id="class_count" value={cookie.class_count} min={1} max={1000} onChange={
 			(event) => {
-				setCookie("class_count", format(event));
+				setCookie("class_count", format(event), { maxAge: cookie_date });
+				setCookie("user_number", input_format(Number(cookie.user_number), 1, Number(event.target.value), true));
 			}
 		} />
 
@@ -51,9 +53,9 @@ const Setting = () => {
 		<button style={{ color: "red" }} onClick={
 			() => {
 				if (confirm("設定(Cookie)は全て消されます。\n本当によろしいですか?")) {
-					removeCookie("dp_count");
-					removeCookie("user_number");
-					removeCookie("class_count");
+					for (let i = 0; i < cookie_list.length; ++i) {
+						removeCookie(cookie_list[i]);
+					}
 					location.href = "/school-hits/";
 				}
 			}
