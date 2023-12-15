8cc1fa75236aa6fe0c3ef5bbaec71b39a5190016
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-15 12:17:20 +0900

    ⚰️ 使わないコードを削除

--- src/func/cookie.ts
+++ src/func/cookie.ts
@@ -1,18 +1,5 @@
 import Cookies from "js-cookie";
 
-// import { cookie_date } from "../variable/cookie";
-
-/**
- * Cookieをデフォルトの設定にする
- * @param force 強制的に実行するかどうか(規定値: false)
- * @deprecated 使わない方がい
- */
-export const init_cookie = (force: boolean = false): void => {
-	if (force || Cookies.get('dp_count') === undefined) {
-		Cookies.set('dp_count', '5', { expires: 1 });
-	}
-}
-
 /**
  * 未設定のCookieを`alert`でお知らせする。
  */
