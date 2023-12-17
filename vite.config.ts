e066980ae355c5ef88241a33e04805586fb8592f
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-17 15:12:52 +0900

    🚨 大きいファイルを分割するように

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/school-hits/'
+  base: '/school-hits/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
