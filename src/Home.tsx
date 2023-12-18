216978f6a9e5425a8138a9452f07aef82a81ddc6
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-18 21:53:35 +0900

    🎨 設定を実行時に整形するように

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,13 +7,34 @@ import { useCookies } from 'react-cookie';
 import { is_after_pm5 } from './func/date';
 import { check_cookie } from './func/cookie';
 import { dp_run } from './func/dp';
+import { input_format } from './func/user_input';
 import { cookie_date } from './variable/cookie';
 
-const Home = () => {
-	const [cookies, setCookie] = useCookies(["dp_count"]);
+interface CookieSetting {
+	name: "class_count" | "user_number",
+	max: number,
+	min: number,
+}
 
-	if (cookies.dp_count === undefined) {
-		setCookie('dp_count', 5, { maxAge: cookie_date });
+const Home = () => {
+	const [cookies, setCookie, removeCookie] = useCookies(["class_count", "user_number"]);
+
+	const format_cookie = () => {
+		const cookie_list: CookieSetting[] = [
+			{ name: "class_count", min: 1, max: 1000 },
+			{ name: "user_number", min: 1, max: Number(cookies.class_count ?? 3776) },
+		];
+
+		console.log(cookie_list);
+		for (let i = 0; i < cookie_list.length; ++i) {
+			console.log(cookie_list[i].name, cookies[cookie_list[i].name]);
+			if (cookies[cookie_list[i].name] === undefined) {
+				setCookie(cookie_list[i].name, String(cookie_list[i].min), { maxAge: cookie_date });
+			} else {
+				removeCookie(cookie_list[i].name);
+				setCookie(cookie_list[i].name, String(input_format(Number(cookies[cookie_list[i].name]), cookie_list[i].min, cookie_list[i].max)), { maxAge: cookie_date });
+			}
+		}
 	}
 
 	return (
@@ -29,8 +50,9 @@ const Home = () => {
 			<br />
 			<br />
 			<button onClick={() => {
+				format_cookie();
 				if (check_cookie()) {
-					console.log(dp_run());
+					console.log(dp_run(Number(cookies.class_count)));
 				}
 			}}>Check</button>
 		</>
--- src/Setting.tsx
+++ src/Setting.tsx
@@ -1,18 +1,12 @@
 
 import { Helmet } from 'react-helmet-async';
 import { useCookies } from 'react-cookie';
-import React from 'react';
 
 import './css/App.css'
 import { input_format } from './func/user_input';
 import { cookie_date } from './variable/cookie';
 
-const cookie_list = ["dp_count", "user_number", "class_count"];
-
-// input_formatの簡略化
-const format = (value: React.ChangeEvent<HTMLInputElement>, integer: boolean = true): number => {
-	return input_format(Number(value.target.value), Number(value.target.min), Number(value.target.max), integer);
-}
+const cookie_list = ["user_number", "class_count"];
 
 const Setting = () => {
 	const [cookie, setCookie, removeCookie] = useCookies(cookie_list);
@@ -27,7 +21,7 @@ const Setting = () => {
 		<label htmlFor="user_number">あなたの出席番号</label>
 		<input type="number" id="user_number" value={cookie.user_number} min={1} max={Number(cookie.class_count ?? 3776)} disabled={cookie.class_count === undefined} onChange={
 			(event) => {
-				setCookie("user_number", format(event), { maxAge: cookie_date });
+				setCookie("user_number", event.target.value, { maxAge: cookie_date });
 			}
 		} />
 
@@ -36,7 +30,7 @@ const Setting = () => {
 		<label htmlFor="class_count">クラスの人数</label>
 		<input type="number" id="class_count" value={cookie.class_count} min={1} max={1000} onChange={
 			(event) => {
-				setCookie("class_count", format(event), { maxAge: cookie_date });
+				setCookie("class_count", event.target.value, { maxAge: cookie_date });
 				setCookie("user_number", input_format(Number(cookie.user_number), 1, Number(event.target.value), true));
 			}
 		} />
