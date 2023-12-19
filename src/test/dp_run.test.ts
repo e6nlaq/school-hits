6669f9db14cba5e94596052f1e57e2f678090622
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-19 19:14:59 +0900

    ✅ `dp_run`テスト追加

---
+++ src/test/dp_run.test.ts
@@ -0,0 +1,52 @@
+
+import dayjs from "dayjs";
+import * as math from "mathjs";
+import { Random, MersenneTwister19937 } from "random-js";
+
+import { dp_run } from "../func/dp";
+import { get_month_day, is_after_pm5 } from "../func/date";
+import { random_count } from "./test_setting";
+
+const random = new Random(MersenneTwister19937.autoSeed());
+
+describe('roop', () => {
+	test.concurrent('length', () => {
+		for (let i = 1; i <= 34; ++i) {
+			const [m, d] = get_month_day(dayjs(is_after_pm5(dayjs())));
+			const plus = Number(m > i) + Number(d > i);
+
+			const result = dp_run(i);
+			expect(Object.keys(result).length).toBeLessThanOrEqual(i + plus);
+		}
+	});
+
+	test.concurrent('dates', () => {
+		for (let m = 1; m <= 12; ++m) {
+			for (let d = 1; d <= 20; ++d) {
+				const i = random.integer(1, 5);
+				const plus = Number(m > i) + Number(d > i);
+
+				const result = dp_run(i, dayjs().month(m).day(d));
+				expect(Object.keys(result).length).toBeLessThanOrEqual(i + plus);
+			}
+		}
+	});
+});
+
+describe('error', () => {
+	test.concurrent('minus', () => {
+		for (let i = 0; i < random_count; ++i) {
+			expect(dp_run(random.integer(Number.MIN_SAFE_INTEGER, -1))).toThrow(RangeError);
+		}
+	});
+
+	test.concurrent('float', () => {
+		for (let i = 0; i < random_count; ++i) {
+			let val = random.real(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+
+			if (math.isInteger(val)) val = math.add(val, 0.1);
+
+			expect(dp_run(val)).toThrow(RangeError);
+		}
+	});
+});
