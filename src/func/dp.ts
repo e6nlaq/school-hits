6b62c58ddb91efdfad298dd4a0439e4417d19713
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-19 19:14:43 +0900

    ✨ エラーの追加

--- src/func/dp.ts
+++ src/func/dp.ts
@@ -10,11 +10,15 @@ interface dp_type {
 	[key: number]: number;
 }
 
-export const dp_run = (class_count: number) => {
+export const dp_run = (class_count: number, date: dayjs.Dayjs = dayjs()): dp_type => {
+	if (!math.isInteger(class_count) || math.isNegative(class_count)) {
+		throw new RangeError(`引数は非負整数である必要があります。(値: ${class_count})`);
+	}
+
 	// dp初期化
 	let dp: dp_type = {};
 
-	const [m, d] = get_month_day(dayjs());
+	const [m, d] = get_month_day(date);
 	dp = { [m]: 0, [d]: 0 };
 
 	const q = new Queue<number>();
@@ -36,22 +40,33 @@ export const dp_run = (class_count: number) => {
 				let result = func(A, B);
 				let value = math.chain(math.max(dp[A], dp[B])).add(func_dp[func_at][1] as number).done();
 
-				// 絶対に答えにならない値を省く
+				// *****************************
+				// 絶対に答えにならない値を省く・整形
+				// *****************************
+
+				// 0未満
 				if (result < 0) {
 					value += 0.5;
 					result = math.abs(result);
 				}
 
+				// クラスの人数より多い
 				if (result > class_count) {
 					value += 0.5;
 					result = math.mod(result, class_count);
+					if (result === 0) {
+						result = class_count;
+					}
 				}
 
+				// 小数
 				if (!math.isInteger(result)) {
 					value += 0.5;
 					result = math.round(result);
 				}
 
+				// 0・NaN・Infinity
+				if (result === 0 || Number.isNaN(result) || result === Infinity) continue;
 
 				if (dp[result] === undefined) {
 					dp[result] = value;
