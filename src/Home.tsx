2bb5c7564a61fb967d35e0faffe7e9f78f6dc6ef
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-19 21:46:47 +0900

    ✨ 視覚的な変化の導入

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,13 @@
 
 import dayjs from 'dayjs';
-import './css/App.css'
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useCookies } from 'react-cookie';
 
+import './css/App.css'
 import { is_after_pm5 } from './func/date';
 import { check_cookie } from './func/cookie';
-import { dp_run } from './func/dp';
+import { dp_run, dp_type } from './func/dp';
 import { input_format } from './func/user_input';
 import { cookie_date } from './variable/cookie';
 
@@ -18,16 +19,20 @@ interface CookieSetting {
 
 const Home = () => {
 	const [cookies, setCookie, removeCookie] = useCookies(["class_count", "user_number"]);
+	const [result, setResult] = useState<dp_type>({});
+	const [run_date, setRunDate] = useState<dayjs.Dayjs>(dayjs());
 
 	const format_cookie = () => {
 		const cookie_list: CookieSetting[] = [
-			{ name: "class_count", min: 1, max: 1000 },
+			{ name: "class_count", min: 1, max: 100 },
 			{ name: "user_number", min: 1, max: Number(cookies.class_count ?? 3776) },
 		];
 
-		console.log(cookie_list);
 		for (let i = 0; i < cookie_list.length; ++i) {
-			console.log(cookie_list[i].name, cookies[cookie_list[i].name]);
+			if (Number(cookies[cookie_list[i].name]) >= cookie_list[i].min && Number(cookies[cookie_list[i].name]) <= cookie_list[i].max) {
+				continue;
+			}
+
 			if (cookies[cookie_list[i].name] === undefined) {
 				setCookie(cookie_list[i].name, String(cookie_list[i].min), { maxAge: cookie_date });
 			} else {
@@ -46,15 +51,20 @@ const Home = () => {
 			<h1>School Hit</h1>
 			<br />
 
-			<input type="date" id="check_date" defaultValue={is_after_pm5(dayjs())} />
+			<input type="date" id="check_date" defaultValue={is_after_pm5(dayjs())} onChange={(event) => {
+				setRunDate(dayjs(event.target.value));
+			}} />
 			<br />
 			<br />
 			<button onClick={() => {
+				setResult({});
 				format_cookie();
 				if (check_cookie()) {
-					console.log(dp_run(Number(cookies.class_count)));
+					setResult(dp_run(Number(cookies.class_count), run_date));
 				}
 			}}>Check</button>
+
+			<h1 id='result'>{result[Number(cookies.user_number)]}</h1>
 		</>
 	)
 }
