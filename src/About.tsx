5f89626d901ceb532c06180c39832a2fabad3e88
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-12-30 12:26:59 +0900

    🎨 外部リンクを追加

--- src/About.tsx
+++ src/About.tsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@geist-ui/react';
+import { Github, FileText } from '@geist-ui/icons';
+
 import './css/about.css';
 
 const About = () => {
@@ -7,9 +11,47 @@ const About = () => {
 
 			<h2>使い方</h2>
 			<div className='about'>
-				<p>1. (初回のみ) Settingタブから設定をする</p>
-				<p>2. HomeタブからCheckボタンを押す</p>
+				<p>
+					1. (初回のみ) <Link to='/setting'>Settingタブ</Link>
+					から設定をする
+				</p>
+				<p>
+					2. <Link to='/'>Homeタブ</Link>に飛び、
+					チェックしたい日付を設定する
+				</p>
+				<p>3. Checkボタンを押して実行!</p>
 			</div>
+
+			<h2>外部リンク</h2>
+
+			<Button
+				icon={<Github />}
+				type='secondary'
+				auto
+				ghost
+				placeholder={undefined}
+				onClick={() => open('https://github.com/e6nlaq/school-hits')}
+				className='link'
+				style={{ margin: '5px' }}
+			>
+				GitHub
+			</Button>
+
+			<Button
+				type='success'
+				auto
+				ghost
+				placeholder={undefined}
+				style={{ margin: '5px' }}
+				icon={<FileText />}
+				onClick={() =>
+					open(
+						'https://github.com/e6nlaq/school-hits/blob/main/LICENSE'
+					)
+				}
+			>
+				License
+			</Button>
 		</>
 	);
 };
